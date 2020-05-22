0591ee3063be605ff8aac2bec9b248abec2c8519
Yicun Hou <yicunhou@bu.edu>
2020-05-22 12:19:56 -0400

    Add Router between login and register

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -2,10 +2,10 @@ import React from 'react';
 import logo from '../assets/logo.svg';
 
 export function Topbar() {
-    return (
-        <header className={"App-header"}>
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Around</h1>
-        </header>
-    );
+  return (
+    <header className={"App-header"}>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Around</h1>
+    </header>
+  );
 }
