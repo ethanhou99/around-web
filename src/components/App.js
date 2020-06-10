08cf66fdaa2f7e26d3ddc74ff38f32866b61b730
Yicun Hou <yicunhou@bu.edu>
2020-06-09 21:04:18 -0400

    Update login method.

--- src/components/App.js
+++ src/components/App.js
@@ -2,35 +2,32 @@ import React from 'react';
 import { TopBar } from './TopBar';
 import { Main } from './Main';
 import { TOKEN_KEY } from '../constants';
+
 import '../styles/App.css';
 
 class App extends React.Component {
-  state = {
-    isLoggedIn: Boolean(localStorage.getItem(TOKEN_KEY)),
-  }
+ state = {
+   isLoggedIn: Boolean(localStorage.getItem(TOKEN_KEY)),
+ }
 
-  handleLogin = (token) => {
-    localStorage.setItem(TOKEN_KEY, token);
-    this.setState({
-      isLoggedIn: true,
-    });
-  }
+ handleLoginSucceed = (token) => {
+   localStorage.setItem(TOKEN_KEY, token)
+   this.setState({ isLoggedIn: true });
+ }
 
-  handleLogout = () => {
-    localStorage.removeItem(TOKEN_KEY);
-    this.setState({
-      isLoggedIn: false,
-    });
-  }
+ handleLogout = () => {
+   localStorage.removeItem(TOKEN_KEY);
+   this.setState({ isLoggedIn: false });
+ }
 
-  render() {
-    return (
-      <div className="App">
-        <TopBar isLoggedIn={this.state.isLoggedIn} handleLogout={this.handleLogout} />
-        <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin} />
-      </div>
-    );
-  }
+ render() {
+   return (
+     <div className="App">
+       <TopBar handleLogout={this.handleLogout} isLoggedIn={this.state.isLoggedIn}/>
+       <Main handleLoginSucceed={this.handleLoginSucceed} isLoggedIn={this.state.isLoggedIn}/>
+     </div>
+   );
+ }
 }
 
 export default App;
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
+export const API_ROOT = 'http://35.223.84.64:8080';
 export const TOKEN_KEY = 'TOKEN_KEY';
 export const GEO_OPTIONS = {
   enableHighAccuracy: true,
